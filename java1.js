4d625285a14272c5d21ed1284dd2f9e55aa19c1f
Kaio Barbosa <kaiobarbosa0694@gmail.com>
2025-11-10 11:13:28 -0300

    subindo os arquivos java

---
+++ java1.js
@@ -0,0 +1,19 @@
+let nome = "Pedro";
+let idade = "15"
+
+console.log("Meu nome é:" +nome); 
+console.log("Minha idade é:" +idade); 
+
+
+
+
+
+let a = 10;
+let b = 3;
+
+console.log("A soma é:" +(a + b));
+console.log(" A subtração é:" + (a - b));
+console.log(" A multiplicação é:" + (a * b));
+console.log(" A divisão é:" +(a / b));
+console.log("A porcentagem é:" + (a % b));
+console.log("A exponencialçao é:" + (a ** b));
---
+++ java3.js
@@ -0,0 +1,10 @@
+let valor_produto = 100.00
+let desconto = valor_produto * 0.10
+let percentual = (valor_produto * desconto) / 100;
+
+
+console.log("Quando de desconto: " + percentual + " %")
+console.log("O Valor do desconto: " + desconto + " R$" )
+console.log("O preço do produto Original é: " + valor_produto + " R$")
+console.log("O valor do produto com descoto é: " + (valor_produto - desconto) + " R$")
+
